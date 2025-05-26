b3b54a6ddfac886dd8a1421b2111bdfdd244d13f
Georgiana-Constantina Aileni <156085841+ailenigeorgiana27@users.noreply.github.com>
2025-05-26 18:43:37 +0300

    Add files via upload

---
+++ Database-Management-Systems/Lab1/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿using System.Drawing;
+using System.Net.Mime;
+using System.Windows.Forms;
+
+namespace Lab1sgdb
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            dataGridViewParent = new DataGridView();
+             dataGridViewChild = new DataGridView();
+             label1 = new Label();
+             label2 = new Label();
+             label3 = new Label();
+             txtDenumire = new TextBox();
+             txtCantitate = new TextBox();
+             txtPret = new TextBox();
+             btnAdaugaProdus = new Button();
+             btnStergeProdus = new Button();
+             btnActualizeazaProdus = new Button();
+            ((System.ComponentModel.ISupportInitialize)( dataGridViewParent)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)( dataGridViewChild)).BeginInit();
+             SuspendLayout();
+            // 
+            // dataGridViewParent
+            // 
+             dataGridViewParent.BackgroundColor = Color.LightBlue;
+             dataGridViewParent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+             dataGridViewParent.GridColor = Color.LightBlue;
+             dataGridViewParent.Location = new Point(44, 14);
+             dataGridViewParent.Margin = new Padding(2, 2, 2, 2);
+             dataGridViewParent.Name = "dataGridViewParent";
+             dataGridViewParent.RowHeadersWidth = 51;
+             dataGridViewParent.RowTemplate.Height = 29;
+             dataGridViewParent.Size = new Size(416, 130);
+             dataGridViewParent.TabIndex = 0;
+             dataGridViewParent.CellClick += dataGridViewParent_CellClick;
+             dataGridViewParent.CellContentClick += dataGridViewParent_CellContentClick;
+            // 
+            // dataGridViewChild
+            // 
+            dataGridViewChild.BackgroundColor = Color.LightBlue;
+             dataGridViewChild.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+             dataGridViewChild.GridColor = Color.LightBlue;
+             dataGridViewChild.Location = new Point(44, 161);
+             dataGridViewChild.Margin = new Padding(2, 2, 2, 2);
+             dataGridViewChild.Name = "dataGridViewChild";
+             dataGridViewChild.RowHeadersWidth = 51;
+             dataGridViewChild.RowTemplate.Height = 29;
+             dataGridViewChild.Size = new Size(416, 130);
+             dataGridViewChild.TabIndex = 1;
+             dataGridViewChild.CellClick += DataGridViewChild_CellClick;
+             dataGridViewChild.CellContentClick += dataGridViewChild_CellContentClick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+             label1.BackColor = Color.LightBlue;
+             label1.Font = new Font("Sylfaen", 10.2F);
+             label1.Location = new Point(87, 312);
+             label1.Margin = new Padding(2, 0, 2, 0);
+             label1.Name = "label1";
+             label1.Size = new Size(63, 18);
+             label1.TabIndex = 1;
+             label1.Text = "Denumire";
+            // 
+            // label2
+            // 
+             label2.AutoSize = true;
+             label2.BackColor = Color.LightBlue;
+             label2.Font = new Font("Sylfaen", 10.2F);
+             label2.Location = new Point(87, 336);
+             label2.Margin = new Padding(2, 0, 2, 0);
+             label2.Name = "label2";
+             label2.Size = new Size(57, 18);
+             label2.TabIndex = 2;
+             label2.Text = "Cantitate";
+            // 
+            // label3
+            // 
+             label3.AutoSize = true;
+             label3.BackColor = Color.LightBlue;
+             label3.Font = new Font("Sylfaen", 10.2F);
+             label3.Location = new Point(87, 360);
+             label3.Margin = new Padding(2, 0, 2, 0);
+             label3.Name = "label3";
+             label3.Size = new Size(31, 18);
+             label3.TabIndex = 3;
+             label3.Text = "Pret";
+            // 
+            // txtDenumire
+            // 
+             txtDenumire.Font = new Font("Sylfaen", 10.2F);
+             txtDenumire.Location = new Point(164, 307);
+             txtDenumire.Margin = new Padding(2, 2, 2, 2);
+             txtDenumire.Name = "txtDenumire";
+             txtDenumire.Size = new Size(248, 25);
+             txtDenumire.TabIndex = 2;
+            // 
+            // txtCantitate
+            // 
+             txtCantitate.Font = new Font("Sylfaen", 10.2F);
+             txtCantitate.Location = new Point(164, 334);
+             txtCantitate.Margin = new Padding(2, 2, 2, 2);
+             txtCantitate.Name = "txtCantitate";
+             txtCantitate.Size = new Size(248, 25);
+             txtCantitate.TabIndex = 3;
+            // 
+            // txtPret
+            // 
+             txtPret.Font = new Font("Sylfaen", 10.2F);
+             txtPret.Location = new Point(164, 361);
+             txtPret.Margin = new Padding(2, 2, 2, 2);
+             txtPret.Name = "txtPret";
+             txtPret.Size = new Size(248, 25);
+             txtPret.TabIndex = 4;
+            // 
+            // btnAdaugaProdus
+            // 
+             btnAdaugaProdus.BackColor = Color.SkyBlue;
+             btnAdaugaProdus.Font = new Font("Sylfaen", 10.2F);
+             btnAdaugaProdus.Location = new Point(88, 410);
+             btnAdaugaProdus.Margin = new Padding(2, 2, 2, 2);
+             btnAdaugaProdus.Name = "btnAdaugaProdus";
+             btnAdaugaProdus.Size = new Size(322, 26);
+             btnAdaugaProdus.TabIndex = 5;
+             btnAdaugaProdus.Text = "Adaugă produsul";
+             btnAdaugaProdus.UseVisualStyleBackColor = false;
+             btnAdaugaProdus.Click += btnAdaugaProdus_Click;
+            // 
+            // btnStergeProdus
+            // 
+            btnStergeProdus.BackColor = Color.SkyBlue;
+             btnStergeProdus.Font = new Font("Sylfaen", 10.2F);
+             btnStergeProdus.Location = new Point(88, 440);
+             btnStergeProdus.Margin = new Padding(2, 2, 2, 2);
+             btnStergeProdus.Name = "btnStergeProdus";
+             btnStergeProdus.Size = new Size(322, 26);
+             btnStergeProdus.TabIndex = 6;
+             btnStergeProdus.Text = "Șterge produsul";
+             btnStergeProdus.UseVisualStyleBackColor = false;
+             btnStergeProdus.Click += btnStergeProdus_Click;
+            // 
+            // btnActualizeazaProdus
+            // 
+             btnActualizeazaProdus.BackColor = Color.SkyBlue;
+             btnActualizeazaProdus.Font = new Font("Sylfaen", 10.2F);
+             btnActualizeazaProdus.Location = new Point(88, 470);
+             btnActualizeazaProdus.Margin = new Padding(2, 2, 2, 2);
+             btnActualizeazaProdus.Name = "btnActualizeazaProdus";
+             btnActualizeazaProdus.Size = new Size(322, 26);
+             btnActualizeazaProdus.TabIndex = 7;
+             btnActualizeazaProdus.Text = "Actualizează produsul";
+             btnActualizeazaProdus.UseVisualStyleBackColor = false;
+            btnActualizeazaProdus.Click += btnActualizeazaProdus_Click;
+            // 
+            // Form1
+            // 
+             AutoScaleDimensions = new SizeF(6F, 13F);
+             AutoScaleMode = AutoScaleMode.Font;
+             ClientSize = new Size(493, 524);
+             Controls.Add( btnActualizeazaProdus);
+             Controls.Add( btnStergeProdus);
+             Controls.Add( btnAdaugaProdus);
+             Controls.Add( txtDenumire);
+             Controls.Add( txtCantitate);
+             Controls.Add( txtPret);
+             Controls.Add( label3);
+             Controls.Add( label2);
+             Controls.Add( label1);
+             Controls.Add( dataGridViewChild);
+             Controls.Add( dataGridViewParent);
+             Margin = new Padding(2, 2, 2, 2);
+             Name = "Patiserie";
+             Text = "Patiserie";
+             Load += Patiserie_Load;
+            ((System.ComponentModel.ISupportInitialize)( dataGridViewParent)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)( dataGridViewChild)).EndInit();
+             ResumeLayout(false);
+             PerformLayout();
+
+        }
+
+      
+
+        #endregion
+
+        private DataGridView dataGridViewParent;
+        private DataGridView dataGridViewChild;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox txtDenumire;
+        private TextBox txtCantitate;
+        private TextBox txtPret;
+        private Button btnAdaugaProdus;
+        private Button btnStergeProdus;
+        private Button btnActualizeazaProdus;
+
+    }
+}
+
---
+++ Database-Management-Systems/Lab1/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+
+
+namespace Lab1sgdb
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = @"Server=PC\SQLEXPRESS; Database=Patiserie_SGBD; Integrated Security=true; TrustServerCertificate=true;";
+
+        private DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter = new SqlDataAdapter();
+        SqlDataAdapter childAdapter = new SqlDataAdapter();
+        BindingSource parentBS = new BindingSource();
+        BindingSource childBS = new BindingSource();
+
+        private int produsSelectat = 0;
+        private int tipSelectat = 0;
+        private string denumireProdus;
+        private string cantitateProdus;
+        private string pretProdus;
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        public void refreshDataGridView()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Tipuri;", connection);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Produse;", connection);
+                    ds.Clear();
+                    parentAdapter.Fill(ds, "Tipuri");
+                    childAdapter.Fill(ds, "Produse");
+                    parentBS.DataSource = ds.Tables["Tipuri"];
+                    dataGridViewParent.DataSource = parentBS;
+                    dataGridViewChild.DataSource = childBS;
+
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void dataGridViewParent_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewParent.Rows[e.RowIndex];
+                int.TryParse(row.Cells[0].Value.ToString(), out tipSelectat);
+            }
+
+            txtDenumire.Text = "";
+            txtCantitate.Text = "";
+            txtPret.Text = "";
+        }
+
+        private void DataGridViewChild_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex >= 0 && e.ColumnIndex >= 0)
+            {
+                DataGridViewRow row = dataGridViewChild.Rows[e.RowIndex];
+                int.TryParse(row.Cells[0].Value.ToString(), out produsSelectat);
+                denumireProdus = row.Cells[1].Value.ToString();
+                cantitateProdus = row.Cells[2].Value.ToString();
+                pretProdus = row.Cells[3].Value.ToString();
+
+            }
+
+            txtDenumire.Text = denumireProdus;
+            txtCantitate.Text = cantitateProdus;
+            txtPret.Text = pretProdus;
+        }
+
+        private void btnAdaugaProdus_Click(object sender, EventArgs e)
+        {
+            if (tipSelectat == 0)
+            {
+                MessageBox.Show("ALEGETI UN TIP PENTRU PRODUS!!!");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand insertCommand =
+                            new SqlCommand(
+                                "INSERT INTO Produse(Denumire, Cantitate, Pret, " +
+                                "idTip) VALUES (@Denumire, @Cantitate, @Pret, @idTip);", connection);
+                        insertCommand.Parameters.AddWithValue("@Denumire", txtDenumire.Text);
+                        insertCommand.Parameters.AddWithValue("@Cantitate", txtCantitate.Text);
+                        insertCommand.Parameters.AddWithValue("@Pret", txtPret.Text);
+                        insertCommand.Parameters.AddWithValue("@idTip", tipSelectat);
+
+                        int insertRowCount = insertCommand.ExecuteNonQuery();
+                        MessageBox.Show("Produsul a fost adaugat cu succes!");
+                        txtDenumire.Text = "";
+                        txtCantitate.Text = "";
+                        txtPret.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        public void btnStergeProdus_Click(object sender, EventArgs e)
+        {
+            if (produsSelectat == 0)
+            {
+                MessageBox.Show("ALEGETI UN PRODUS!!!");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand deleteCommand = new SqlCommand("DELETE FROM Produse WHERE idProduse = @idProduse;",
+                            connection);
+                        deleteCommand.Parameters.AddWithValue("@idProduse", produsSelectat);
+                        int deleteRowCount = deleteCommand.ExecuteNonQuery();
+                        MessageBox.Show("Produsul a fost sters");
+                        txtDenumire.Text = "";
+                        txtCantitate.Text = "";
+                        txtPret.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message); 
+                }
+            }
+        }
+
+        private void btnActualizeazaProdus_Click(object sender, EventArgs e)
+        {
+            if (produsSelectat == 0)
+            {
+                MessageBox.Show("ALEGETI UN PRODUS!!!");
+            }
+            else
+            {
+                try
+                {
+                    using (SqlConnection connection = new SqlConnection(connectionString))
+                    {
+                        connection.Open();
+                        SqlCommand updateCommand =
+                            new SqlCommand(
+                                "UPDATE Produse SET Denumire=@Denumire, Cantitate=@Cantitate, Pret=@Pret WHERE idProduse=@idProduse;",
+                                connection);
+                        updateCommand.Parameters.AddWithValue("@Denumire", txtDenumire.Text);
+                        updateCommand.Parameters.AddWithValue("@Cantitate", txtCantitate.Text);
+                        updateCommand.Parameters.AddWithValue("@Pret", txtPret.Text);
+                        updateCommand.Parameters.AddWithValue("@idProduse", produsSelectat);
+                        int updateRowCount = updateCommand.ExecuteNonQuery();
+                        MessageBox.Show("Produsul a fost actualizat");
+                        txtDenumire.Text = "";
+                        txtCantitate.Text = "";
+                        txtPret.Text = "";
+                        refreshDataGridView();
+                        connection.Close();
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void Patiserie_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Tipuri;", connection);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Produse;", connection);
+                    parentAdapter.Fill(ds, "Tipuri");
+                    childAdapter.Fill(ds, "Produse");
+                    parentBS.DataSource = ds.Tables["Tipuri"];
+                    dataGridViewParent.DataSource = parentBS;
+                    DataColumn parentColumn = ds.Tables["Tipuri"].Columns["idTip"];
+                    DataColumn childColumn = ds.Tables["Produse"].Columns["idTip"];
+                    DataRelation relation = new DataRelation("FK__Produse__idTip__3B75D760", parentColumn, childColumn);
+                    ds.Relations.Add(relation);
+                    childBS.DataSource = parentBS;
+                    childBS.DataMember = "FK__Produse__idTip__3B75D760";
+                    dataGridViewChild.DataSource = childBS;
+
+                    connection.Close();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void dataGridViewChild_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridViewParent_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+
+
+    }
+
